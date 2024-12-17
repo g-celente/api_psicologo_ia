d67c77679c5a1cbb1d846049314204b869bfec56
guilherme_celente <guilherme.celente@outlook.com>
2024-12-17 15:10:57 -0300

    att

--- src/controllers/main/pagamentoController.js
+++ src/controllers/main/pagamentoController.js
@@ -3,6 +3,9 @@
 const pagamentoController = {
     compraRealizada: async (req, res) => {
         console.log(req.body)
+
+        res.status(200).json({ message: 'Notificação recebida com sucesso' });
+
     }
 }
 
